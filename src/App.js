dc1e947996475059f42bac76156f364ffa074080
allisonLMT <59849533+allisonLMT@users.noreply.github.com>
2022-05-09 20:45:28 -0700

    Initial set up
    -adding basic files
    -includes basic set up of the API call in App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+
+  const [animalInput, setAnimalInput] = useState("");
+  const [result, setResult] = useState();
+
+  async function getResponse(url, parameters) {
+    
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify(parameters)
+
+    });
+    return response.json()
+  }
+
+  function onSubmit(event) {
+    event.preventDefault();
+
+    const url = 'https://api.openai.com/v1/engines/text-curie-001/completions';
+    const parameters = {
+      prompt: "Write a poem about a dog wearing skis",
+      temperature: 0.5,
+      max_tokens: 64,
+      top_p: 1.0,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+    };
+     
+    getResponse(url, parameters)
+    .then(data => {
+      console.log(data)
+    });
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="animal"
+            placeholder="Enter an animal"
+            value={animalInput}
+            onChange={(e) => setAnimalInput(e.target.value)}
+          />
+          <input type="submit" value="Generate pet names" />
+        </form>
+        <div >{result}</div>
+     
     </div>
   );
 }
---
+++ src/api/generate.js
@@ -0,0 +1,36 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (res) {
+    const response = await openai.createCompletion("text-davinci-002", {
+        prompt: "Say this is a test",
+        temperature: 0,
+        max_tokens: 6,
+    });
+    res.status(200).json({ result: response.data.choices[0].text });
+}
+
+// export default async function (req, res) {
+//   const completion = await openai.createCompletion("text-davinci-002", {
+//     prompt: generatePrompt(req.body.animal),
+//     temperature: 0.6,
+//   });
+//   res.status(200).json({ result: completion.data.choices[0].text });
+// }
+
+// function generatePrompt(animal) {
+//   const capitalizedAnimal =
+//     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+//   return `Suggest three names for an animal that is a superhero.
+
+//   Animal: Cat
+//   Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+//   Animal: Dog
+//   Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+//   Animal: ${capitalizedAnimal}
+//   Names:`;
+// }
