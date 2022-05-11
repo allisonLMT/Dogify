e81cc0e00895fac78afdee32c25d9e7b54b6aed8
allisonLMT <59849533+allisonLMT@users.noreply.github.com>
2022-05-11 09:04:52 -0700

    App.js clean up
    -fixed the wonky tab spacing
    -no real changes to code, just spacing

--- src/App.js
+++ src/App.js
@@ -6,91 +6,85 @@ import './index.scss';
 
 function App() {
 
-  const [subjectInput, setSubjectInput] = useState('');
-  const [results, setResults] = useState([]);
-  const [selectedEngine, setSelectedEngine] = useState('text-curie-001');
+    const [subjectInput, setSubjectInput] = useState('');
+    const [results, setResults] = useState([]);
+    const [selectedEngine, setSelectedEngine] = useState('text-curie-001');
   
+    async function getResponse(url, parameters) {
+        const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
+        },
+        body: JSON.stringify(parameters)
+        });
+        return response.json()
+    }
 
-  async function getResponse(url, parameters) {
-    
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
-      },
-      body: JSON.stringify(parameters)
-
-    });
-    return response.json()
-  }
-
-  function onSubmit(event) {
-    event.preventDefault();
-    const url = `https://api.openai.com/v1/engines/${selectedEngine}/completions`;
-    const parameters = {
-      prompt: generatePrompt(subjectInput),
-      temperature: 0.5,
-      max_tokens: 64,
-      top_p: 1.0,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
-    };
+    function onSubmit(event) {
+        event.preventDefault();
+        const url = `https://api.openai.com/v1/engines/${selectedEngine}/completions`;
+        const parameters = {
+        prompt: generatePrompt(subjectInput),
+        temperature: 0.5,
+        max_tokens: 64,
+        top_p: 1.0,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
+        };
      
-    getResponse(url, parameters)
-    .then(data => {
-      //add a key value pair to store the prompt with it's result
-      data.prompt = parameters.prompt;
-      console.log(data)
-      //takes the previous result array, and concats the new response, then updates the result state
-      var updatedResults = results.concat(data)
-      setResults(updatedResults);
-    });
-  }
+        getResponse(url, parameters)
+        .then(data => {
+            //add a key value pair to store the prompt with it's result
+            data.prompt = parameters.prompt;
+            console.log(data)
+            //takes the previous result array, and concats the new response, then updates the result state
+            var updatedResults = results.concat(data)
+            setResults(updatedResults);
+        });
+    }
 
-  function generatePrompt(input) {
-    //takes the user input and generates a prompt to create a poem
-    const goldenPrompt = `Write a story about a Goldendoodle and ${input}`;
-
-    return  goldenPrompt;
-  }
+    function generatePrompt(input) {
+        //takes the user input and generates a prompt to create a poem
+        const goldenPrompt = `Write a story about a Goldendoodle and ${input}`;
+        return  goldenPrompt;
+    }
  
-  function onRadioChange(event) {
-    console.log("radio change")
-    setSelectedEngine(event.target.value);
-  }
+    function onRadioChange(event) {
+        console.log("radio change")
+        setSelectedEngine(event.target.value);
+    }
 
-  return (
+    return (
     <div className='page-container'>
-      <div className='content-wrap'>
-        <h1>Dogify</h1>
-        <form onSubmit={onSubmit}>
-            <input
-              type="text"
-              name="object"
-              placeholder="Enter an object"
-              value={subjectInput}
-              // API allows maxlength = 1000
-              maxLength="100"
-              onChange={(e) => setSubjectInput(e.target.value)}
-            />
+        <div className='content-wrap'>
+            <h1>Dogify</h1>
+            <form onSubmit={onSubmit}>
+                <input
+                    type="text"
+                    name="object"
+                    placeholder="Enter an object"
+                    value={subjectInput}
+                    // API allows maxlength = 1000
+                    maxLength="100"
+                    onChange={(e) => setSubjectInput(e.target.value)}
+                />
+                <p>Please select the engine:</p>
+                <EngineRadio engine='text-curie-001' selectedEngine={selectedEngine} onRadioChange={onRadioChange} />
+                <EngineRadio engine='text-davinci-002' selectedEngine={selectedEngine} onRadioChange={onRadioChange} />
+                <EngineRadio engine='text-babbage-001' selectedEngine={selectedEngine} onRadioChange={onRadioChange} />
+                <EngineRadio engine='text-ada-001' selectedEngine={selectedEngine} onRadioChange={onRadioChange} />
 
-            <p>Please select the engine:</p>
-            <EngineRadio engine='text-curie-001' selectedEngine={selectedEngine} onRadioChange={onRadioChange} />
-            <EngineRadio engine='text-davinci-002' selectedEngine={selectedEngine} onRadioChange={onRadioChange} />
-            <EngineRadio engine='text-babbage-001' selectedEngine={selectedEngine} onRadioChange={onRadioChange} />
-            <EngineRadio engine='text-ada-001' selectedEngine={selectedEngine} onRadioChange={onRadioChange} />
-
-            <input type="submit" value="Dogify!" id="submit"/>
-        </form>
+                <input type="submit" value="Dogify!" id="submit"/>
+            </form>
         
-          {results.length > 0 &&
-            <section>
-              <Results results={results} /> 
-            </section>
-          }
-      
-      </div>
+            {results.length > 0 &&
+                <section>
+                    <Results results={results} /> 
+                </section>
+            }
+        </div>
       <Footer />
     </div>
   );
