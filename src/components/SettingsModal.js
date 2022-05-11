baa27d02dfb07f846b555923f31f3779a0378c51
allisonLMT <59849533+allisonLMT@users.noreply.github.com>
2022-05-11 14:45:21 -0700

    Create SettingsModal.js
    -basic set up of imports and functional component

---
+++ src/components/SettingsModal.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Modal from 'react-modal';
+
+
+function SettingsModal() {
+
+    return (
+        <div>
+
+        </div>
+    )
+
+}
+
+export default SettingsModal;
