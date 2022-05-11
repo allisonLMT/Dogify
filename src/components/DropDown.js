f0385bf40e4ae360216fb524cf019caa5f6d3932
allisonLMT <59849533+allisonLMT@users.noreply.github.com>
2022-05-11 11:05:51 -0700

    drop down styling
    -the label no longer wraps the select (they now stack on top of each other, rather than inline)
    -created a module for the dropdown styling and added it to the component

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import styles from '../styles/dropDown.module.scss'
 
 
 function DropDown({ onDropDownChange }) {
 
 
     return (
-        <label htmlFor='engine'>Select an engine:
-            <select name="engine" id="engine" onChange={onDropDownChange}>
+        <div>
+            <label htmlFor='engine' id={styles.engineLabel}>Select an engine:</label>
+            <select name="engine" className={styles.engine} onChange={onDropDownChange}>
                 <option value="text-curie-001" >text-curie-001</option>
                 <option value="text-davinci-002" >text-davinci-002</option>
                 <option value="text-babbage-001" >text-babbage-001</option>
                 <option value="text-ada-001" >text-ada-001</option>
             </select>
-        </label>
+        </div>
+        
     )
 }
 
