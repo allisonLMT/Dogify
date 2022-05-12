66b0d58a43ff97ebe94434246b81c44aa5930802
allisonLMT <59849533+allisonLMT@users.noreply.github.com>
2022-05-12 10:50:56 -0700

    sanitization of user input
    -as the user enters input, a new function handles the input. It sets the userInput (which is for the visible prompt and text input display) the same as the user entered it. However, the input that is sent to the API is sanitized.

--- src/App.js
+++ src/App.js
@@ -7,9 +7,18 @@ import './index.scss';
 
 function App() {
 
+   
+
+    //input from text field
     const [userInput, setUserInput] = useState('');
+    //sanitized input from text field
+    const [sanitizedInput, setSanitizedInput] = useState('');
+    //results returned from the API, stored newest to oldest
     const [results, setResults] = useState([]);
+    //engine selected from settings modal
     const [selectedEngine, setSelectedEngine] = useState('text-curie-001');
+
+    //const [prompts, setPrompts] = useState({})
   
     async function getResponse(url, parameters) {
         const response = await fetch(url, {
@@ -23,23 +32,41 @@ function App() {
         return response.json()
     }
 
-    function onSubmit(event) {
+    function handleInput(e) {
+        console.log("handling")
+        //sanitizes the input as it's entered
+        //display/prompt is kept as the user has entered it, but the input sent to the API is sanitized
+        setUserInput(e.target.value)
+
+        //replaces all characters that are NOT alphanumeric, apostrophe or space and removes it (replaces it with nothing (''))
+        const regex = /[^a-z0-9' ]/gi;
+        const sanitizedInputValue = e.target.value.replace(regex, '');
+       
+        setSanitizedInput(sanitizedInputValue);
+
+    }
+
+    function onSubmit() {
         console.log("submitted")
-        event.preventDefault();
+        
+        const prompts = generatePrompts();
+        console.log("sanitized: " + sanitizedInput)
+        console.log(prompts)
+
         const url = `https://api.openai.com/v1/engines/${selectedEngine}/completions`;
         const parameters = {
-        prompt: generatePrompt(userInput),
-        temperature: 0.5,
-        max_tokens: 64,
-        top_p: 1.0,
-        frequency_penalty: 0.0,
-        presence_penalty: 0.0,
+            prompt: prompts.sanitized,
+            temperature: 0.5,
+            max_tokens: 64,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
         };
      
         getResponse(url, parameters)
         .then(data => {
             //add a key value pair to store the prompt with it's result
-            data.prompt = parameters.prompt;
+            data.prompt = prompts.asIs;
             
             //takes the existing result array, and concats the new response
             //then reverses the array (so when mapped, they are newest to oldest) and updates the result state
@@ -49,42 +76,42 @@ function App() {
        
     }
 
-    function randomNum(arr) {
-        return Math.floor(Math.random()*arr.length);
-    }
-
-    function shuffleItem(userInput) {
-        const items = ['a green pencil sharpener', 'a handlebar mustache', 'a monocle and tophat', 'a campfire and marshmallows', 'some unicycles and a clown', 'a pair of shoes', 'a cat and a toucan', 'some warm cookies and milk','a dark and stormy night', 'a hotdog with mustard'];
-        console.log("selecting items")
-        var item;
-        //if the user hasn't added input, use a random item instead
-        if (userInput === '') {
-            item = items[randomNum(items)];
-            setUserInput(item);
-        } else {
-            return userInput;
-        }
-
-        return item
-    }
-
-    function generatePrompt(userInput) {
-        console.log("generating prompt")
-        //randomly select a dog breed from the list
-        const breeds = ['Goldendoodle', 'French Bulldog', 'Corgi', 'Daschund', 'Chihuahua', 'Basset Hound', 'Great Dane', 'German Shephard', 'Pug', 'Bernese Mountain Dog', 'Beagle', 'Golden Retriever', 'Cocker Spaniel'];
-        const randomBreed = breeds[randomNum(breeds)];
-
-        const input = shuffleItem(userInput);
-
-        //takes the user input and generates a prompt to create a story about the input and the selected breed
-        const dogPrompt = `Write a story about a ${randomBreed} and ${input}.`;
-        return  dogPrompt;
-    }
-
     function onDropDownChange(event) {
         console.log("dropdownchange")
         setSelectedEngine(event.target.value);
     }
+
+    function randomNum(arr) {
+        return Math.floor(Math.random()*arr.length);
+    }
+
+    function shuffleItem() {
+        const items = ['a green pencil sharpener', 'a handlebar mustache', 'a monocle and tophat', 'a campfire and marshmallows', 'a pair of shoes', 'a cat and a pom-pom crinkle ball', 'some warm cookies and milk','a dark and stormy night', 'a hotdog with ketchup and relish', 'an apple pie and vanilla ice cream', 'a walk along the beach', 'a cup of tea and a good book'];
+        console.log("shuffling items")
+
+        const item = items[randomNum(items)];
+        setUserInput(item);
+        setSanitizedInput(item);
+
+    }
+
+    function generatePrompts() {
+        console.log("generating prompt")
+
+        //randomly select a dog breed from the list
+        const breeds = ['Goldendoodle', 'French Bulldog', 'Corgi', 'Daschund', 'Chihuahua', 'Basset Hound', 'Great Dane', 'German Shephard', 'Pug', 'Bernese Mountain Dog', 'Beagle', 'Golden Retriever', 'Cocker Spaniel'];
+        const randomBreed = breeds[randomNum(breeds)];
+       
+        //generate two prompts with sanitizedInput and with userInput as-is (this is used for the stored prompt)
+        const prompts = {
+            sanitized: `Write a story about a ${randomBreed} and ${sanitizedInput}.`,
+            asIs: `Write a story about a ${randomBreed} and ${userInput}.`,
+        }
+       
+        return  prompts;
+    }
+
+    
 
     return (
     <div className='page-container'>
@@ -95,7 +122,7 @@ function App() {
                 <p>Dogify will write a story about a dog and whatever else you choose!</p>
                 <p>Not sure? Hit Shuffle! And don't worry, we'll pick a dog for you.</p>
             </section>
-            <section>
+            <section className='form'>
                 <div className='textShuffle'>
                     <input
                         type="text"
@@ -105,12 +132,14 @@ function App() {
                         value={userInput}
                         // API allows maxlength = 1000
                         maxLength="100"
-                        onChange={(e) => setUserInput(e.target.value)}
+                        // onChange={(e) => setUserInput(e.target.value)}
+                        onChange={(e) => handleInput(e)}
                     />
                     <button className="material-icons" onClick={shuffleItem}>shuffle</button>
                 </div>
                 <div className='submitSettings'>
                     <SettingsModal onDropDownChange={onDropDownChange}/>
+                    {/* <input type="submit" value="Dogify Now!" id="submit" onClick={handleInput}/> */}
                     <input type="submit" value="Dogify Now!" id="submit" onClick={onSubmit}/>
                 </div>
             </section>
