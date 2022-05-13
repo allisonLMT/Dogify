15016629e286d902a85e6b4990a065f9612831e6
allisonLMT <59849533+allisonLMT@users.noreply.github.com>
2022-05-13 11:23:50 -0700

    Heart svgs, storage functions
    -changed heart to inline SVG for ease of styling and hover states
    -started adding functions to localStorage file

--- src/components/localStorageFavorites.js
+++ src/components/localStorageFavorites.js
@@ -1,2 +1,13 @@
 //will need functions to add to faves and remove
 //will need a way to tell if it's in faves (so the heart can conditionally render)
+
+
+function addFavorite() {
+
+}
+
+function removeFavorite() {
+
+}
+
+export { addFavorite, removeFavorite };
