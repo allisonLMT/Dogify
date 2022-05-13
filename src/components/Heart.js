f388f18a8b88e98adec7630c933cfb4bc2d97d09
allisonLMT <59849533+allisonLMT@users.noreply.github.com>
2022-05-13 10:01:41 -0700

    added hearts
    -added svgs for both hearts
    -added heart to OneResult
    -no onClick yet, just image in place

--- src/components/Heart.js
+++ src/components/Heart.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import emptyHeart from '../images/emptyHeart.svg';
+import filledHeart from '../images/filledHeart.svg';
 
 
 function Heart() {
     //conditionally render the type of heart (empty or full) depending on if it's in faves or not (will need a way to tell)
     //empty heart will add it to faves onClick and turn to full. Clicking on full heart will remove and change to empty.
 
-    
+
     return (
 
         <div>
-
+            <img src={emptyHeart} alt="not favorite" />
         </div>
     )
 }
