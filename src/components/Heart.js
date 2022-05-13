3b822e554e19771d1c5903b8b73af2bd7414560b
allisonLMT <59849533+allisonLMT@users.noreply.github.com>
2022-05-13 09:52:24 -0700

    set up heart component and localStorageFaves
    -basic set up of component
    -added some notes about what needs to be done

---
+++ src/components/Heart.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+function Heart() {
+    //conditionally render the type of heart (empty or full) depending on if it's in faves or not (will need a way to tell)
+    //empty heart will add it to faves onClick and turn to full. Clicking on full heart will remove and change to empty.
+
+    
+    return (
+
+        <div>
+
+        </div>
+    )
+}
+
+export default Heart;
---
+++ src/components/localStorageFavorites.js
@@ -0,0 +1,2 @@
+//will need functions to add to faves and remove
+//will need a way to tell if it's in faves (so the heart can conditionally render)
