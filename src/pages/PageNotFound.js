aa76f9728255ed3d8095b337288ee1303e0f0552
allisonLMT <59849533+allisonLMT@users.noreply.github.com>
2022-05-14 17:10:59 -0700

    Styling of 404 and buttons
    -positioning of text on 404 and Favorites (so that they are the same when there are no favorites to display)
    -changed the hover color of the translucent buttons

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -3,6 +3,7 @@ import { HashLink } from 'react-router-hash-link';
 import NavMenu from '../components/NavMenu';
 import styles from '../styles/pageNotFound.module.scss';
 import Footer from '../components/Footer';
+import classNames from 'classnames';
 
 function PageNotFound () {
 
@@ -11,9 +12,9 @@ function PageNotFound () {
         <div className='page-container'>
             <HashLink to="#content" className="skip-to-content-link">Skip to content</HashLink>
             <NavMenu />
-                <div className='content-wrap' id="content">
-                    <h2 className={styles.notFound}>404 Error</h2>
-                    <h3 className={styles.notFound}>Page not found.</h3>
+                <div className={classNames('content-wrap', styles.notFound)} id="content">
+                    <h2>404 Error</h2>
+                    <p>Page not found.</p>
                 </div>
             <Footer />
         </div>
